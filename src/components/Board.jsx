280eb743fc947851574570e4f37a2d26c8428e67
Malik Elmessiry <malik.messiry@gmail.com>
2025-06-23 21:48:16 +0200

    Add initial data structure and components for board display

---
+++ src/components/Board.jsx
@@ -0,0 +1,10 @@
+
+
+const Board = ({ title, owner }) => {
+
+    return (
+        <li className="">{`${title} - ${owner}`}</li>
+    );
+};
+
+export default Board;
---
+++ src/components/BoardList.jsx
@@ -0,0 +1,20 @@
+import Board from "./Board";
+
+
+const BoardList = props => {
+    const getBoardList = props.boards.map((board) => {
+        return (
+            <Board
+                key={board.id}
+                id={board.id}
+                title={board.title}
+                owner={board.owner}
+            />
+        );
+    });
+    return <ul className="">{getBoardList}</ul>;
+};
+    
+
+
+export default BoardList;
