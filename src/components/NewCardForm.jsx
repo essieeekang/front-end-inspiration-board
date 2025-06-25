3c38788a13094c4098b079a8c5951fdf37cabd6c
Esther Kang <estherkang.j@gmail.com>
2025-06-25 07:59:45 -0700

    adds rules for eslint config and cleans up formatting

--- src/components/NewCardForm.jsx
+++ src/components/NewCardForm.jsx
@@ -1,28 +1,28 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const NewCardForm = ({ onCardAdd }) => {
-    const [newCardData, setNewCardData] = useState({
-        message: ''
-    });
+  const [newCardData, setNewCardData] = useState({
+    message: ''
+  });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onCardAdd(newCardData);
-        setNewCardData({ message: ''})
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCardAdd(newCardData);
+    setNewCardData({ message: ''});
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type='text'
-                placeholder="Type your message here"
-                value={newCardData.message}
-                required
-                onChange={(e) => setNewCardData({...newCardData, message: e.target.value})}
-            />
-            <button type="submit">Add Card</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder="Type your message here"
+        value={newCardData.message}
+        required
+        onChange={(e) => setNewCardData({...newCardData, message: e.target.value})}
+      />
+      <button type="submit">Add Card</button>
+    </form>
+  );
 };
 
 export default NewCardForm;
