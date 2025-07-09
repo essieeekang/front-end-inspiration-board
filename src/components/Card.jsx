0cdc05444045829e501dfb196b8d39d80ffdf7d4
jenny xu <jingwenxu13@gmail.com>
2025-07-09 11:38:23 -0700

    new_card_color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,16 +6,11 @@ const Card = ({id, message, likes, onLikeCard, onDeleteCard}) => {
   // Function to generate a random color
   const getRandomColor = () => {
     const colors = [
-      '#B793C8', // Mauve Mist
-      '#C08081', // Old Rose
-      '#DEA5A4', // Blossom Pink
-      '#D8BFD8', // Thistle
-      '#C4A3BF', // Lilac Grey
-      '#A186BE', // Wisteria
-      '#F5CBA7', // Apricot Cream
-      '#F4A7B9', // Peach Blush
-      '#A77CA0', // Dusty Plum
-      '#ECD9A7', // Soft Gold
+      ' #db96b9', // Rose Quartz
+      'e4a8b9', // Pink Champagne
+      '#c8a8d5', // Light Coral
+      '#d2ccf2', // Peach Puff
+      '#f2d2cc', // Light Salmon
     ];
 
     const randomIndex = Math.floor(Math.random() * colors.length);
